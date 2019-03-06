66bcb3e952c933a434cf4a69e0ec1217be5dbe20
lcheng1 <3242609+lcheng1@users.noreply.github.com>
2019-03-05 22:29:50 -0800

    Updates

---
+++ precache-manifest.3f1f744147e68ed2e0234128b3d7cf99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50dcbbc762ce94f83b9f",
+    "url": "/telemetrics-view/static/css/main.e40848de.chunk.css"
+  },
+  {
+    "revision": "50dcbbc762ce94f83b9f",
+    "url": "/telemetrics-view/static/js/main.50dcbbc7.chunk.js"
+  },
+  {
+    "revision": "ec5354cf454a9026edda",
+    "url": "/telemetrics-view/static/js/runtime~main.ec5354cf.js"
+  },
+  {
+    "revision": "04b46f0340294d1df194",
+    "url": "/telemetrics-view/static/css/2.e729780a.chunk.css"
+  },
+  {
+    "revision": "04b46f0340294d1df194",
+    "url": "/telemetrics-view/static/js/2.04b46f03.chunk.js"
+  },
+  {
+    "revision": "8c7a25fe8720aa887af2448fda569d92",
+    "url": "/telemetrics-view/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/telemetrics-view/precache-manifest.fa7b9634a1f5ef5310781f5458e348a5.js"
+  "/telemetrics-view/precache-manifest.3f1f744147e68ed2e0234128b3d7cf99.js"
 );
 
 workbox.clientsClaim();
