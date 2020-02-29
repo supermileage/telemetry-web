bffa8899c8fdb7c38f5005dde01246b793452778
Leslie Cheng <leslie.cheng5@gmail.com>
2020-02-29 09:31:45 -0800

    New proto datasets.

--- src/config/datasets.js
+++ src/config/datasets.js
@@ -4,6 +4,12 @@ import UrbanLocation from "./datasets/Urban/Location.js";
 
 import ProtoSpeed from "./datasets/Proto/Speed.js";
 import ProtoLocation from "./datasets/Proto/Location.js";
+import ProtoECT from "./datasets/Proto/ECT.js";
+import ProtoIAT from "./datasets/Proto/IAT.js";
+import ProtoRPM from "./datasets/Proto/RPM.js";
+import ProtoUBADC from "./datasets/Proto/UBADC.js";
+import ProtoO2S from "./datasets/Proto/O2S.js";
+import ProtoSPARK from "./datasets/Proto/SPARK.js";
 
 let datasets = {
   Urban: [
@@ -12,6 +18,12 @@ let datasets = {
     UrbanLocation
   ],
   Proto: [
+    ProtoECT,
+    ProtoIAT,
+    ProtoRPM,
+    ProtoUBADC,
+    ProtoO2S,
+    ProtoSPARK,
     ProtoSpeed,
     ProtoLocation
   ]
